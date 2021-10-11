39d553e0704f419f151ddfac84c6dffe9561d838
Noah Cohen Kalafut <themathmaster3000@gmail.com>
2021-10-11 18:28:23 -0500

    Hao-Yu's Code

--- part_2b/main.py
+++ part_2b/main.py
@@ -1,56 +1,43 @@
-import argparse
-import copy
-import json
-import logging
 import os
-import random
-from time import perf_counter
-
-import model as mdl
-import numpy as np
 import torch
-import torch.distributed as dd
+import json
+import copy
+import numpy as np
+from torchvision import datasets, transforms
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torchvision import datasets, transforms
-
-
-# Parse arguments
-parser = argparse.ArgumentParser()
-parser.add_argument('--master-ip', dest='master_ip', type=str)
-parser.add_argument('--master-port', dest='master_port', default=6585, type=int)
-parser.add_argument('--num-nodes', dest='num_nodes', type=int)
-parser.add_argument('--rank', dest='rank', type=int)
-args = parser.parse_args()
-MASTER_IP = args.master_ip
-MASTER_PORT = args.master_port
-NUM_NODES = args.num_nodes
-RANK = args.rank
-
-# Distributed setup
-print('Initializing...')
-MAIN_RANK = 0
-dd.init_process_group('gloo',
-                      init_method=f'tcp://{MASTER_IP}:{MASTER_PORT}',
-                      rank=RANK,
-                      world_size=NUM_NODES)
-WORLD_SIZE = torch.distributed.get_world_size()
-LOCAL_SIZE = torch.cuda.device_count()
-
-# Seeding
-np.random.seed(42)
-torch.manual_seed(42)
-
-# Setup
-device = 'cpu'
+import logging
+import random
+import model as mdl
+from torch import distributed as dist
+import sys
+import argparse
+import numpy
+import time
+device = "cpu"
 torch.set_num_threads(4)
-BATCH_SIZE = int(256 / NUM_NODES)
-CLI_INTERVAL = 20
-SAMPLE_INTERVAL = (1, 40) # Not right-side inclusive
+
+batch_size = 256 # batch for one node
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--master-ip", help = "Maters's ip")
+parser.add_argument("--num-nodes", type = int, help = "Node's num")
+parser.add_argument("--rank", type = int, help = "rank")
 
 
-def train_model(model, train_loader, optimizer, criterion, epoch):
+
+def dist_setup(rank, world_size, master):
+    dist.init_process_group(backend = "gloo",
+                            init_method = master,
+                            rank = rank,
+                            world_size = world_size)
+
+
+
+        
+
+def train_model(model, train_loader, optimizer, criterion, epoch, world_size, rank):
     """
     model (torch.nn.module): The model created to train
     train_loader (pytorch data loader): Training data loader
@@ -58,39 +45,38 @@ def train_model(model, train_loader, optimizer, criterion, epoch):
     criterion (nn.CrossEntropyLoss) : Loss function used to train the network
     epoch (int): Current epoch number
     """
+    running_loss = 0.0
+    # remember to exit the train loop at end of the epoch
     for batch_idx, (data, target) in enumerate(train_loader):
-        # Add timer
-        if batch_idx == SAMPLE_INTERVAL[0]:
-            start_time = perf_counter()
-        elif batch_idx == SAMPLE_INTERVAL[1]:
-            duration = perf_counter() - start_time
-            avg_time = duration / (SAMPLE_INTERVAL[1] - SAMPLE_INTERVAL[0])
-            print(f'Average batch times {SAMPLE_INTERVAL}: {avg_time}')
+        # Your code goes here!
+        
 
-        # Make data usable
-        data, target = data.to(device), target.to(device)
-
-        # Forward
-        logits = model(data)
-
-        # Backward
         optimizer.zero_grad()
-        loss = criterion(logits, target)
+        outputs = model(data)
+        loss = criterion(outputs, target)
         loss.backward()
-
-        for param in model.parameters():
-            grad = param.grad
-            dd.all_reduce(grad)
-            grad /= WORLD_SIZE
-            # `assert torch.equal(grad, param.grad)` passes, so just setting
-            # `grad` is alright
-
+        
+        
+        # Traverse all the layers
+        for parameter in model.parameters():
+            
+            grad = parameter.grad
+            # Doing the all_reduce
+            dist.all_reduce(grad, op=dist.ReduceOp.SUM)
+            grad /= world_size
+            
+            parameter.grad = grad
+        
+        
+             
         optimizer.step()
 
-        # Print every `CLI_INTERVAL` batches on all ranks
-        if batch_idx % CLI_INTERVAL == 0:
-            print(f'Epoch: {epoch}, Batch: {batch_idx}, Local loss: {loss}')
+        running_loss += loss.item()
+        if batch_idx % 20 == 19:
+            print('[%d, %5d] loss: %.3f' %(epoch + 1, batch_idx + 1, running_loss / 20))
+            running_loss = 0.0
 
+    return None
 
 def test_model(model, test_loader, criterion):
     model.eval()
@@ -108,9 +94,17 @@ def test_model(model, test_loader, criterion):
     print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
             test_loss, correct, len(test_loader.dataset),
             100. * correct / len(test_loader.dataset)))
-
+            
 
 def main():
+    # Parse the argument 
+    args = parser.parse_args()
+    world_size = args.num_nodes
+    rank = args.rank
+    master = args.master_ip
+
+    dist_setup(rank, world_size, master)
+
     normalize = transforms.Normalize(mean=[x/255.0 for x in [125.3, 123.0, 113.9]],
                                 std=[x/255.0 for x in [63.0, 62.1, 66.7]])
     transform_train = transforms.Compose([
@@ -125,33 +119,54 @@ def main():
             normalize])
     training_set = datasets.CIFAR10(root="./data", train=True,
                                                 download=True, transform=transform_train)
+    # Parttion the datae
+    train_sampler = torch.utils.data.distributed.DistributedSampler(training_set,
+                num_replicas = world_size,
+                rank = rank
+            )
 
-    # Add distributed sampling
-    distributed_sampler = torch.utils.data.distributed.DistributedSampler(training_set)
+    # Setup the random seed
+    def seed_worker(worker_id):
+        worker_seed = torch.initial_seed() % 2**32
+        numpy.random.seed(worker_seed)
+        random.seed(worker_seed)
+    g = torch.Generator()
+    g.manual_seed(2048)
+     
     train_loader = torch.utils.data.DataLoader(training_set,
-                                                    num_workers=2,
-                                                    batch_size=BATCH_SIZE,
-                                                    sampler=distributed_sampler,
+                                                    num_workers=world_size,
+                                                    batch_size=int(batch_size/4),
+                                                    sampler=train_sampler,
+                                                    worker_init_fn=seed_worker,
+                                                    generator=g,
+                                                    shuffle=(train_sampler is None),
                                                     pin_memory=True)
-
     test_set = datasets.CIFAR10(root="./data", train=False,
                                 download=True, transform=transform_test)
 
     test_loader = torch.utils.data.DataLoader(test_set,
                                               num_workers=2,
-                                              batch_size=BATCH_SIZE,
+                                              batch_size=batch_size,
                                               shuffle=False,
                                               pin_memory=True)
     training_criterion = torch.nn.CrossEntropyLoss().to(device)
+    
 
     model = mdl.VGG11()
     model.to(device)
     optimizer = optim.SGD(model.parameters(), lr=0.1,
                           momentum=0.9, weight_decay=0.0001)
+
+    start = time.time()
     # running training for one epoch
     for epoch in range(1):
-        train_model(model, train_loader, optimizer, training_criterion, epoch)
+        train_sampler.set_epoch(epoch)
+        train_model(model, train_loader, optimizer, training_criterion, epoch, world_size, rank)
         test_model(model, test_loader, training_criterion)
+    
+    end = time.time()
+    print("time :" + str(end - start))
+
 
 if __name__ == "__main__":
     main()
